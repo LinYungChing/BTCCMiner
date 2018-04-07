9ca7d11b2adb0fd8fa4aca0cf7d78fc312e9e052
Ending2015a <joe85812@yahoo.com.tw>
2018-04-07 20:06:29 +0800

    a

--- main.cpp
+++ main.cpp
@@ -8,6 +8,23 @@ int main(int argc, char **argv)
     btc::StratumPool pool("cn.stratum.slushpool.com", 3333,
                         "trashe725.cppminer", "fuckyou123");
 
+
+    btc::BTCBlock block;
+
+    while(true)
+    {
+        bool newblock = false;
+
+        if(pool.detectNewBlock())
+        {
+            newblock = true;
+            block = pool.getNewBlock();
+        }
+
+        //solve your block
+        pool.submit(block);
+    }
+
     pool.close();
     return 0;
 }
